24627527c25a6ab3c4b70394aa955ee969e0dae5
pupil <pupil@ivkhk.ee>
2022-10-07 15:16:14 +0300

    user can add a few authors per book

--- src/jktv21oopsample/App.java
+++ src/jktv21oopsample/App.java
@@ -1,11 +1,14 @@
 package jktv21oopsample;
 
+import java.util.Arrays;
 import myclasses.Book;
 import tools.Tools;
+import java.util.Scanner;
 
 public class App {
     public void run(){
         Tools tools = new Tools();
+        Scanner scanner = new Scanner(System.in);
         // using public static Author createAuthor(String firstname, String lastname)
         Book book3 = new Book();
         book3.setTitle("Learning JavaEE7"); 
@@ -15,6 +18,27 @@ public class App {
         Book book4 = tools.createBook("New Book Title");
         book4.addAuthor(tools.createAuthor("Tolik", "Pruzhinkin"));
         System.out.println(book4.toString());
+        // add array with books
+        Book[] books = new Book[5];
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Book title " + (i+1) + " : ");
+            String title = scanner.nextLine();
+            Book book = new Book();
+            book.setTitle(title);
+            System.out.println("How many authors: ");
+            int countAuthorsInBook = scanner.nextInt();
+            scanner.nextLine();
+            for (int j = 0; j < countAuthorsInBook; j++) {
+            System.out.println("Author name" + (j+1) + " : ");
+            String firstname = scanner.nextLine();
+            System.out.println("Author lastname" + (j+1) + " : ");
+            String lastname = scanner.nextLine();
+            book.addAuthor(tools.createAuthor(firstname, lastname));
+            }
+            
+            books[i] = book;
+        }
+        System.out.println(Arrays.toString(books));
     }
     
     
