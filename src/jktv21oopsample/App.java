6f8a25d6bbbb714a13a20c381bf62ee48dc7a71d
DmitProg <dmonk30@gmail.com>
2022-10-09 22:09:08 +0300

    ...

--- src/jktv21oopsample/App.java
+++ src/jktv21oopsample/App.java
@@ -10,31 +10,33 @@ public class App {
         Tools tools = new Tools();
         Scanner scanner = new Scanner(System.in);
         // using public static Author createAuthor(String firstname, String lastname)
-        Book book3 = new Book();
-        book3.setTitle("Learning JavaEE7"); 
-        book3.addAuthor(tools.createAuthor("Antonio", "Gonsalves"));
-        System.out.println(book3.toString());
+//        Book book3 = new Book();
+//        book3.setTitle("Learning JavaEE7"); 
+//        book3.addAuthor(tools.createAuthor("Antonio", "Gonsalves"));
+//        System.out.println(book3.toString());
         //--------- using private Book createBook(String bookTitle) -------------------------------------------
-        Book book4 = tools.createBook("New Book Title");
-        book4.addAuthor(tools.createAuthor("Tolik", "Pruzhinkin"));
-        System.out.println(book4.toString());
+//        Book book4 = tools.createBook("New Book Title");
+//        book4.addAuthor(tools.createAuthor("Tolik", "Pruzhinkin"));
+//        System.out.println(book4.toString());
+
         // add array with books
         Book[] books = new Book[5];
         for (int i = 0; i < 5; i++) {
             System.out.println("Book title " + (i+1) + " : ");
             String title = scanner.nextLine();
-            Book book = new Book();
-            book.setTitle(title);
+//            Book book = new Book();
+            tools.createBook(title);
+//            book.setTitle(title);
             System.out.println("How many authors: ");
             int countAuthorsInBook = scanner.nextInt();
             scanner.nextLine();
-            for (int j = 0; j < countAuthorsInBook; j++) {
-            System.out.println("Author name" + (j+1) + " : ");
-            String firstname = scanner.nextLine();
-            System.out.println("Author lastname" + (j+1) + " : ");
-            String lastname = scanner.nextLine();
-            book.addAuthor(tools.createAuthor(firstname, lastname));
-            }
+                for (int j = 0; j < countAuthorsInBook; j++) {
+                System.out.println("Author name " + (j+1) + " - book " + (i+1) + ": ");
+                String firstname = scanner.nextLine();
+                System.out.println("Author lastname " + (j+1) + " - book " + (i+1) + ": ");
+                String lastname = scanner.nextLine();
+                book.addAuthor(tools.createAuthor(firstname, lastname));
+                }
             
             books[i] = book;
         }
--- src/jktv21oopsample/JKTV21OOPSample.java
+++ src/jktv21oopsample/JKTV21OOPSample.java
@@ -5,41 +5,41 @@
 4 Создайте массив из 5 книг
 */
 package jktv21oopsample;
-import java.util.Arrays;
+//import java.util.Arrays;
 
-import myclasses.Author;
-import myclasses.Book;
+//import myclasses.Author;
+//import myclasses.Book;
 
 public class JKTV21OOPSample {
     
     public static void main(String[] args) {
-        Author author1 = new Author();
-        author1.setName("Lev");
-        author1.setLastname("Tolstoi");
-        // Author[] authors1 = new Author[1];
-        // authors1[0] = author1;
-        Book book = new Book();
-        book.setTitle("War and Peace");
-        //book.setAuthors(authors1);    // without method addAuthor in class book
-        book.addAuthor(author1);        // using method addAuthor in class book
-        System.out.println(book.toString());
-        System.out.printf("Book{authors = %s, title = %s}", Arrays.toString(book.getAuthors()),book.getTitle());        // this is using getter in method toString of class Book
-    //------------------------------------------------------------------------------------------
-        Author author2 = new Author();              // create variable. Class variable = new constructor
-        Author author3 = new Author();              // create variable
-        author2.setName("Ilja");                    // set value for variable. Object.method(method's parametr)
-        author2.setLastname("Ilf");                 // set value for variable
-        author3.setName("Evgeniy");                 // set value for variable
-        author3.setLastname("Petrov");              // set value for variable
-        // Author[] authors2 = new Author[2];          // create an array                   // without method addAuthor in class book
-        // authors2[0] = author2;                      // write value to index 0 of array   // without method addAuthor in class book
-        // authors2[1] = author3;                      // write value to index 1 of array   // without method addAuthor in class book
-        Book book2 = new Book();                    // create variable
-        book2.setTitle("Twelve chairs");            // set value for variable
-        book2.addAuthor(author2);                   // using method addAuthor in class book
-        book2.addAuthor(author3);                   // using method addAuthor in class book
-        // book2.setAuthors(authors2);                 // set value for variable            // without method addAuthor in class book
-        System.out.println(book2.toString());       // print out. toString - method from class Book
+//        Author author1 = new Author();
+//        author1.setName("Lev");
+//        author1.setLastname("Tolstoi");
+//        // Author[] authors1 = new Author[1];
+//        // authors1[0] = author1;
+//        Book book = new Book();
+//        book.setTitle("War and Peace");
+//        //book.setAuthors(authors1);    // without method addAuthor in class book
+//        book.addAuthor(author1);        // using method addAuthor in class book
+//        System.out.println(book.toString());
+//        System.out.printf("Book{authors = %s, title = %s}", Arrays.toString(book.getAuthors()),book.getTitle());        // this is using getter in method toString of class Book
+//    //------------------------------------------------------------------------------------------
+//        Author author2 = new Author();              // create variable. Class variable = new constructor
+//        Author author3 = new Author();              // create variable
+//        author2.setName("Ilja");                    // set value for variable. Object.method(method's parametr)
+//        author2.setLastname("Ilf");                 // set value for variable
+//        author3.setName("Evgeniy");                 // set value for variable
+//        author3.setLastname("Petrov");              // set value for variable
+//        // Author[] authors2 = new Author[2];          // create an array                   // without method addAuthor in class book
+//        // authors2[0] = author2;                      // write value to index 0 of array   // without method addAuthor in class book
+//        // authors2[1] = author3;                      // write value to index 1 of array   // without method addAuthor in class book
+//        Book book2 = new Book();                    // create variable
+//        book2.setTitle("Twelve chairs");            // set value for variable
+//        book2.addAuthor(author2);                   // using method addAuthor in class book
+//        book2.addAuthor(author3);                   // using method addAuthor in class book
+//        // book2.setAuthors(authors2);                 // set value for variable            // without method addAuthor in class book
+//        System.out.println(book2.toString());       // print out. toString - method from class Book
         //---------------------------------------------------------------------------------------------
         
         // using class App. All above can be transferred to the class App.
